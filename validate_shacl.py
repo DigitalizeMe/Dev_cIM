c6414d2087d157d4b82b10ea0124d40576c52dd0
DigitalizeMe <97295430+DigitalizeMe@users.noreply.github.com>
2025-02-27 14:16:51 +0100

    Update validate_shacl.py

--- validate_shacl.py
+++ validate_shacl.py
@@ -3,6 +3,8 @@ import subprocess
 TBOX_PATH = "OCCP_V0.26.ttl"
 ABOX_PATH = "OCCP_Phase_A_VALID_1.ttl"
 
+JENA_PATH = r"C:\Program Files (x86)\jena-5.2.0\apache-jena\bat\shacl.bat"
+
 # Run Apache Jena SHACL-Validator 
 def validate_shacl(tbox, abox):
     command = [
