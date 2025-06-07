aaee3b47e32c400022aa4b6a8495f79fb3e8ab5e
DigitalizeMe <97295430+DigitalizeMe@users.noreply.github.com>
2025-06-07 22:19:35 +0200

    ocp valid def

--- Test.py
+++ Test.py
@@ -1,7 +1,4 @@
 from rdflib import Graph
-try:
-    g = Graph()
-    g.parse("G:/Promo/Dev_cIM/Onto/TBOX/time.owl", format="turtle")
-    print("time.owl parsed successfully")
-except Exception as e:
-    print(f"Error parsing time.owl: {e}")
+g = Graph()
+g.parse("G:/Promo/Dev_cIM/Onto/ABOX/OCP/OCP_ABOX_12.ttl", format="turtle")
+print("ABOX parsed successfully")
